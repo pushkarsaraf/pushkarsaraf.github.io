93a5d2fa2314b6e369daffd08491a6a5cdc3636b
pushkarsaraf <pusshkar.saraf@gmail.com>
2019-03-19 03:20:48 +0530

    Add files via upload

---
+++ gvs/js/init.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $(function () {
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('#titlex').fitText(1);
+
+    });
+
+})(jQuery);
+// end of jQuery name space
